e0ce98bd1ceab56776c3d7117a1da97800b8c9b6
mansoorcpp <61862243+mansoorcpp@users.noreply.github.com>
2026-03-08 15:51:31 +0100

    Changed and Updated Code
    This mostly uses Callbacks to implement
    functionality but since this is an internal
    DSL this should work? This is Iter1, needs testing.

--- Program1.cs
+++ Program1.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 
 namespace StrategyGameTextbasedPrototype
 {
-    public class ConditionException : Exception
+    public class TurnException : Exception
     {
-        public ConditionException(string message) : base(message)
+        public TurnException(string message) : base(message)
         {
 
         }
@@ -17,46 +15,111 @@ namespace StrategyGameTextbasedPrototype
     class Game
     {
         public string name;
+        public int timesetup;
         Player player1 = new Player();
         Player player2 = new Player();
+        public int turns = 0;
+        Func<Player, Player, bool> winningCond;
+        Func<Player, Player, bool> losingCond;
 
-        List<Rule> winconditions = new List<Rule>();
-        List<Rule> loseconditions = new List<Rule>();
-        public Game createGame(string name, Dictionary<string, Resources> ply1Rsr, Dictionary<string, Resources> ply2Rsr)
+        public List<Decision> thisGameDecisions = new List<Decision>();
+        private void LogStats()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"===== STATS AFTER TURN {turns} =====");
+            this.player1.PrintStats("Player 1");
+            this.player2.PrintStats("Player 2");
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+        }
+        private void CheckEndOfGame()
+        {
+            if (this.winningCond == null || this.losingCond == null)
+                return; // nothing configured yet
+
+            // Convention: winningCond(candidate, opponent)
+            bool p1Won = winningCond(this.player1, this.player2);
+            bool p2Won = winningCond(this.player2, this.player1);
+            bool p1Lost = losingCond(this.player1, this.player2);
+            bool p2Lost = losingCond(this.player2, this.player1);
+
+            if (p1Won || p2Lost)
+            {
+                Console.WriteLine(">>> Player 1 wins!");
+                LogStats();
+                Environment.Exit(0);
+            }
+
+            if (p2Won || p1Lost)
+            {
+                Console.WriteLine(">>> Player 2 wins!");
+                LogStats();
+                Environment.Exit(0);
+            }
+        }
+
+        public Game createGame(string name, int timesetup, Dictionary<string, Resources> ply1Rsr, Dictionary<string, Resources> ply2Rsr, int p1Health, int p2Health)
         {
             this.name = name;
             this.player1.resources = ply1Rsr;
             this.player2.resources = ply2Rsr;
-            return this;
-        }
-
-        public Game playerInitialization(int p1Health, int p2Health)
-        {
+            this.timesetup = timesetup;
             this.player1.Health = p1Health;
             this.player2.Health = p2Health;
             return this;
         }
 
-        public Game setWinCondition(List<Rule> winCond)
+        public Game AssignEachPlayerObjects(Dictionary<string, Resources> ply1MilRsr, Dictionary<string, Resources> ply2MilRsr)
         {
-            this.winconditions = winCond;
+            this.player1.milResources = ply1MilRsr;
+            this.player2.milResources = ply2MilRsr;
             return this;
         }
 
-        public Game setloseCondition(List<Rule> loseCond)
+        public Game SetWinningLosing(Func<Player, Player, bool> WinningConditions, Func<Player, Player, bool> LosingConditions)
         {
-            this.winconditions = loseCond;
+            this.winningCond = WinningConditions;
+            this.losingCond = LosingConditions;
+            return this;
+        }
+        public Game SetDecisions(string DecisionName, Action<Player, Player> DecisionAction)
+        {
+            Decision assignment = new Decision();
+            assignment.DecisionName = DecisionName;
+            assignment.DecisionAction = DecisionAction;
+            this.thisGameDecisions.Add(assignment);
             return this;
         }
 
-        // public Player 
-
-        public Game setAction(string name, Player player, List<Rule> preCond)
+        public Game TakeDecision(int playerCount, string DecisionName)
         {
+            if (this.turns % 2 == 0)
+            {
+                if (playerCount != 1)
+                {
+                    throw new TurnException("Wrong Turn");
+                }
+            }
+            else
+            {
+                if (playerCount != 2)
+                {
+                    throw new TurnException("Wrong Turn");
+                }
+            }
+
+            var decision = thisGameDecisions.Find(d => d.DecisionName == DecisionName);
+            if (decision == null)
+                throw new InvalidOperationException($"Unknown decision '{DecisionName}'");
+
+            decision.DecisionAction(this.player1, this.player2);
+            turns++;
+
+            LogStats();
+            CheckEndOfGame();
+
             return this;
         }
-
-
         public static void Main(string[] args)
         {
         }
@@ -68,12 +131,10 @@ namespace StrategyGameTextbasedPrototype
         private static int playerCount = 0;
 
         public string playerName;
-        public int Health = 100;
+        public int Health;
         public Dictionary<string, Resources> resources = new Dictionary<string, Resources>();
 
-        public Dictionary<string, object> Stats = new Dictionary<string, object>();
-
-        public List<Action> actions = new List<Action>();
+        public Dictionary<string, Resources> milResources = new Dictionary<string, Resources>();
 
         public static int PlayerCount
         {
@@ -83,6 +144,21 @@ namespace StrategyGameTextbasedPrototype
         public Player()
         {
             playerCount++;
+        }
+
+        public void PrintStats(string PlayerName)
+        {
+            Console.WriteLine($"Player: {PlayerName}\n");
+            Console.WriteLine("Resources : \n");
+            foreach (var kvp in this.resources)
+            {
+                Console.WriteLine($"{kvp.Key} {kvp.Value.PrintResource()} \n");
+            }
+            foreach (var bvp in this.milResources)
+            {
+                Console.WriteLine($"{bvp.Key} {bvp.Value.PrintResource()}\n");
+            }
+            Console.WriteLine($"Health of this player {this.Health} \n");
         }
     }
 
@@ -133,85 +209,17 @@ namespace StrategyGameTextbasedPrototype
         {
             return currVal < minVal;
         }
+
+        public string PrintResource()
+        {
+            string str = $"Resource_Name = {this.resourceName} Current_Value = {this.currVal} Max_value = {this.maxVal} Min_value = {this.minVal}";
+            return str;
+        }
     }
-    class Rule
+
+    class Decision
     {
-        public string name;
-        public Player PlayerRef1;
-        public Player PlayerRef2;
-        private List<object> rules = new List<object>();
-
-        public Rule(string name, List<object> obj, Player ply1, Player ply2)
-        {
-            this.PlayerRef1 = ply1;
-            this.PlayerRef1 = ply1;
-            this.name = name;
-            this.rules = obj;
-        }
-
-        public static bool ParseRules(List<Rule> rules)
-        {
-            bool retval = true;
-            foreach (var rule in rules)
-            {
-                retval &= rule.ParseRule();
-            }
-            return retval;
-        }
-        // Example: evaluate all rules with logical AND
-        public bool ParseRule()
-        {
-            bool result = true;
-            foreach (var r in rules)
-            {
-                if (r is Conditions c)
-                    result &= c.ParseCondition();
-                else if (r is bool b)
-                    result &= b;
-                else
-                    throw new ConditionException("Unsupported rule type");
-            }
-            return result;
-        }
+        public string DecisionName;
+        public Action<Player, Player> DecisionAction;
     }
-
-    class Conditions
-    {
-        public string Type;        // unused here but you can keep it
-        public object LeftOperand;
-        public object RightOperand;
-        public string Operand;     // "and" / "or"
-
-        public bool ParseCondition()
-        {
-            bool left = EvaluateOperand(LeftOperand);
-            bool right = EvaluateOperand(RightOperand);
-
-            switch (Operand.Trim().ToLowerInvariant())
-            {
-                case "or":
-                    return left || right;
-                case "and":
-                    return left && right;
-                default:
-                    throw new ConditionException("Invalid operator in condition");
-            }
-        }
-
-        private bool EvaluateOperand(object op)
-        {
-            if (op is Conditions nested)
-                return nested.ParseCondition();       // recursive for nested condition
-            if (op is bool b)
-                return b;
-
-            throw new ConditionException("Operand must be bool or Conditions");
-        }
-    }
-
-    class Action
-    {
-        List<Rule> preconditions = new List<Rule>();
-    }
-
 }
